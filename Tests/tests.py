12179386f072c607fa23edbec0f942f022fa4e9b
Léo Géré <leo.gere@inrae.fr>
2021-09-03 16:50:15 +0200

    Useless: Tests

--- Tests/tests.py
+++ Tests/tests.py
@@ -42,3 +42,44 @@ class CreateDot(Scene):
     def construct(self):
         square = Square()
         self.play(*CreateWithDot(square, Dot()))
+
+
+class Loading(Scene):
+    def construct(self):
+        dot = Dot(color=RED)
+        circle = Circle(2)
+        turn_animation_into_updater(MoveAlongPath(dot, circle, rate_func=linear, run_time=5), cycle=True)
+        path = TracedPath(dot.get_center, dissipating_time=4, stroke_width=[0,18], stroke_color=[YELLOW, RED])
+        self.add(dot)
+        self.add(path)
+        self.wait(30)
+
+class ReducingPath(Scene):
+    def construct(self):
+        dot = Dot()
+        path = TracedPath(dot.get_center, dissipating_time=1, stroke_width=[0,1])
+        self.add(dot, path)
+        self.play(dot.animate.to_corner(DR))
+        self.play(dot.animate.to_corner(UR))
+        self.wait()
+
+class PathProblems(Scene):
+    def construct(self):
+        dot1 = Dot().to_corner(UR)
+        dot2 = Dot().to_edge(RIGHT)
+        dot3 = Dot().to_corner(DR)
+        path1 = TracedPath(dot1.get_center, dissipating_time=2, stroke_opacity=[1, 0])
+        path2 = TracedPath(dot2.get_center, dissipating_time=2, stroke_width=[1, 0])
+        path3 = TracedPath(dot3.get_center, dissipating_time=2, stroke_color=[RED, YELLOW])
+        self.add(dot1, dot2, dot3, path1, path2, path3)
+        self.play(VGroup(dot1, dot2, dot3).animate(run_time=3).to_edge(LEFT))
+        self.wait(3)
+
+class BlueText(Text):
+    def __init__(self, text, **kwargs):
+        super().__init__(text, color=BLUE, **kwargs)
+
+class TestText(Scene):
+    def construct(self):
+        text = BlueText("Hello world!")
+        self.add(text)
